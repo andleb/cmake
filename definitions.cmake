29a1746f2f132b8325b2861762b760cdd79a81f6
Andrej Leban <andrejleban@gmail.com>
2020-03-11 14:01:22 +0100

    Fixed filesystem support.

--- definitions.cmake
+++ definitions.cmake
@@ -6,14 +6,14 @@ set( CMAKE_CXX_STANDARD_REQUIRED ON )
 
 message("COMPILER_ID:${CMAKE_CXX_COMPILER_ID}")
 message("STANDARD:${CMAKE_CXX_STANDARD}")
-message("LINKER:${CMAKE_EXE_LINKER_FLAGS}")
 
+# prefer to set the warning flags in IDE
 if( CMAKE_CXX_COMPILER_ID MATCHES "GNU" )
     message( "USING GCC" )
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -pedantic -Wno-unused-macros")
+    # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -pedantic -Wno-unused-macros")
 
 # for linking against libc++ on linux
-#if qtcreator sets CMAKE_CXX_COMPILER in kit selection
+# if qtcreator sets CMAKE_CXX_COMPILER in kit selection
 elseif( CMAKE_CXX_COMPILER_ID MATCHES "clang" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
     message( "USING CLANG" )
 
@@ -23,24 +23,25 @@ elseif( CMAKE_CXX_COMPILER_ID MATCHES "clang" OR CMAKE_CXX_COMPILER_ID MATCHES "
 #    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++" )
 #    set( CMAKE_EXE_LINKER_FLAGS "-lc++abi" )
    
-    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-unused-macros -Wno-newline-eof\
-    -Wno-exit-time-destructors -Wno-global-constructors -Wno-gnu-zero-variadic-macro-arguments\
-    -Wno-documentation -Wno-shadow -Wno-missing-prototypes -Wno-vla\
-    -Wno-padded" )
+    # set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-unused-macros -Wno-newline-eof\
+    # -Wno-exit-time-destructors -Wno-global-constructors -Wno-gnu-zero-variadic-macro-arguments\
+    # -Wno-documentation -Wno-shadow -Wno-missing-prototypes -Wno-vla\
+    # -Wno-padded" )
 else()
     message( WARNING "DID NOT MATCH A COMPILER!")
     return()
 endif()
 
 # add filesystem support 
-if(CMAKE_EXE_LINKER_FLAGS MATCHES "lc\\+\\+")
+link_libraries(stdc++fs)
+
+if(CMAKE_EXE_LINKER_FLAGS MATCHES "lc\\+\\+" AND NOT CMAKE_EXE_LINKER_FLAGS MATCHES "lc\\+\\+fs")
     message("USING LIBC++")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lc++fs")
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lc++fs")
-else()    	
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lstdc++fs")
+elseif(NOT CMAKE_EXE_LINKER_FLAGS MATCHES "lstdc\\+\\+fs")
 	set( CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lstdc++fs" )
 endif()	
+
 
 message("CMAKE_EXE_LINKER_FLAGS: " ${CMAKE_EXE_LINKER_FLAGS})
 message("CMAKE_CXX_FLAGS: " ${CMAKE_CXX_FLAGS})
